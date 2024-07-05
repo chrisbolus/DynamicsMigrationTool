c2896d319223167dc5555296db9550454aaffcfb
chrisbolus28@gmail.com <chrisbolus28@gmail.com>
2024-07-05 11:13:34 +0100

    Major overhaul of EntityAttribute_AdditionalInfo (formerly Attribute_StagingInfo)

--- DynamicsMigrationTool/MyPluginControl.designer.cs
+++ DynamicsMigrationTool/MyPluginControl.designer.cs
@@ -35,6 +35,8 @@
             this.StagingDatabase_Lbl = new System.Windows.Forms.Label();
             this.About_btn = new System.Windows.Forms.Button();
             this.CreateSrcVwTmpl_Btn = new System.Windows.Forms.Button();
+            this.sourceDBConnection_txtb = new System.Windows.Forms.TextBox();
+            this.SourceDatabase_Lbl = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // CreateStgTbl_Btn
@@ -75,7 +77,7 @@
             // StagingDatabase_Lbl
             // 
             this.StagingDatabase_Lbl.AutoSize = true;
-            this.StagingDatabase_Lbl.Location = new System.Drawing.Point(42, 310);
+            this.StagingDatabase_Lbl.Location = new System.Drawing.Point(42, 313);
             this.StagingDatabase_Lbl.Name = "StagingDatabase_Lbl";
             this.StagingDatabase_Lbl.Size = new System.Drawing.Size(223, 16);
             this.StagingDatabase_Lbl.TabIndex = 9;
@@ -101,10 +103,29 @@
             this.CreateSrcVwTmpl_Btn.UseVisualStyleBackColor = true;
             this.CreateSrcVwTmpl_Btn.Click += new System.EventHandler(this.CreateSrcVwTmpl_Btn_Click);
             // 
+            // sourceDBConnection_txtb
+            // 
+            this.sourceDBConnection_txtb.Location = new System.Drawing.Point(42, 276);
+            this.sourceDBConnection_txtb.Name = "sourceDBConnection_txtb";
+            this.sourceDBConnection_txtb.Size = new System.Drawing.Size(683, 22);
+            this.sourceDBConnection_txtb.TabIndex = 12;
+            this.sourceDBConnection_txtb.TextChanged += new System.EventHandler(this.sourceDBConnection_txtb_TextChanged);
+            // 
+            // SourceDatabase_Lbl
+            // 
+            this.SourceDatabase_Lbl.AutoSize = true;
+            this.SourceDatabase_Lbl.Location = new System.Drawing.Point(42, 257);
+            this.SourceDatabase_Lbl.Name = "SourceDatabase_Lbl";
+            this.SourceDatabase_Lbl.Size = new System.Drawing.Size(220, 16);
+            this.SourceDatabase_Lbl.TabIndex = 13;
+            this.SourceDatabase_Lbl.Text = "Source Database Connection String";
+            // 
             // MyPluginControl
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.SourceDatabase_Lbl);
+            this.Controls.Add(this.sourceDBConnection_txtb);
             this.Controls.Add(this.CreateSrcVwTmpl_Btn);
             this.Controls.Add(this.About_btn);
             this.Controls.Add(this.StagingDatabase_Lbl);
@@ -129,5 +150,7 @@
         private System.Windows.Forms.Label StagingDatabase_Lbl;
         private System.Windows.Forms.Button About_btn;
         private System.Windows.Forms.Button CreateSrcVwTmpl_Btn;
+        private System.Windows.Forms.TextBox sourceDBConnection_txtb;
+        private System.Windows.Forms.Label SourceDatabase_Lbl;
     }
 }
